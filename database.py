d070795a80aaedde3852778d52bc2c84ebce6066
dominikklusak <dom.klusak@gmail.com>
2024-07-24 14:33:01 +0200

    added quasar for frontend

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 def init_db():
     conn = sqlite3.connect('logs.db')
@@ -7,16 +8,18 @@ def init_db():
         CREATE TABLE IF NOT EXISTS logs (
             id TEXT PRIMARY KEY,
             domain TEXT,
-            upload_date TEXT
+            upload_date TEXT,
+            upload_time TEXT
         )
     ''')
     conn.commit()
     conn.close()
 
-def add_log(log_id, domain, upload_date):
+def add_log(log_id, domain, upload_date, upload_time):
     conn = sqlite3.connect('logs.db')
     c = conn.cursor()
-    c.execute('INSERT INTO logs (id, domain, upload_date) VALUES (?, ?, ?)', (log_id, domain, upload_date))
+    c.execute('INSERT INTO logs (id, domain, upload_date, upload_time) VALUES (?, ?, ?, ?)', 
+              (log_id, domain, upload_date, upload_time))
     conn.commit()
     conn.close()
 
